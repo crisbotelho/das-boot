b780ee004df69d9d4db067f8b9863255a356fcee
botelhocris <cristiano.botelho@hotmart.com.br>
2017-08-15 23:21:10 -0300

    Adding unit test
    Adding unit test.

--- das-boot/src/test/java/com/boot/AppTest.java
+++ das-boot/src/test/java/com/boot/AppTest.java
@@ -1,38 +1,25 @@
 package com.boot;
 
+import com.boot.controller.HomeController;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @org.junit.Test
     public void testApp()
     {
-        assertTrue( true );
+        HomeController homeController = new HomeController();
+        String result = homeController.home();
+        assertEquals(result, "Das Boot, reporting for duty!");
     }
 }
